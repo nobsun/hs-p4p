9983f67e1cda0a24f7a7ab69c7266265e1e389c2
Nobuo Yamashita <nobsun@sampou.org>
2018-10-04 23:35:39 +0900

    implement mkCmd

--- src/YouWillAllConform.hs
+++ src/YouWillAllConform.hs
@@ -4,4 +4,19 @@ type Cap = Char
 type Cmd = String
 
 pleaseConform :: [Cap] -> [Cmd]
-pleaseConform = undefined
+pleaseConform = map mkCmd . gonyogonyo
+
+type Pos = Int
+type Range = (Pos, Pos)
+
+mkCmd :: Range -> Cmd
+mkCmd (i, j)
+  | i == j    = showPos i ++ change
+  | otherwise = showPos i ++ "から" ++ showPos j ++ change
+  where
+    showPos pos = show pos ++ "番目"
+    change      = "の人は帽子の向きを替えてください"
+
+gonyogonyo :: [Cap] -> [Range]
+gonyogonyo = undefined
+
--- test/YouWillAllConformSpec.hs
+++ test/YouWillAllConformSpec.hs
@@ -10,25 +10,37 @@ main :: IO ()
 main = hspec spec
 
 -- テスト入力データ
-
 cap1 :: [Cap]
 cap1 = "FFBBBFBBBFFBF"
 cap2 :: [Cap]
 cap2 = "FFBBBFBBBFFFF"
+range1 :: Range
+range1 = (2, 4)
+range2 :: Range
+range2 = (11, 11)
 
 -- テストコード
-
 spec :: Spec
 spec = do
- { describe "pleaseConform"     $ do
-   { it "can make commands people to conform their cap" $ do
-     { pleaseConform cap1 `shouldBe` [ "2番目から4番目の人は帽子の向きを替えてください"
-                                     , "6番目から8番目の人は帽子の向きを替えてください"
-                                     , "11番目の人は帽子の向きを替えてください"
-                                     ]
-     ; pleaseConform cap2 `shouldBe` [ "2番目から4番目の人は帽子の向きを替えてください"
-                                     , "6番目から8番目の人は帽子の向きを替えてください"
-                                     ]
-     }
-   }
- } 
+  { describe "mkCmd"             $ do
+    { context ("when provided with " ++ show range1) $ do
+      { it "can make a command from a range containing more than one positions" $ do
+        { mkCmd range1 `shouldBe` "2番目から4番目の人は帽子の向きを替えてください" }
+      }
+    ; context ("when provided with " ++ show range2) $ do
+      { it "can make a command from a singleton range" $ do
+        { mkCmd range2 `shouldBe` "11番目の人は帽子の向きを替えてください" }
+      }
+    }
+  ; describe "pleaseConform"     $ do
+    { it "can make commands people to conform their cap" $ do
+      { pleaseConform cap1 `shouldBe` [ "2番目から4番目の人は帽子の向きを替えてください"
+                                      , "6番目から8番目の人は帽子の向きを替えてください"
+                                      , "11番目の人は帽子の向きを替えてください"
+                                      ]
+      ; pleaseConform cap2 `shouldBe` [ "2番目から4番目の人は帽子の向きを替えてください"
+                                      , "6番目から8番目の人は帽子の向きを替えてください"
+                                      ]
+      }
+    }
+  } 
