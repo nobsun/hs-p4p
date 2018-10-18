f13c472fbfafa297b061b1756c8c17c036a37c18
Nobuo Yamashita <nobsun@sampou.org>
2018-10-19 07:06:18 +0900

    implement bestTiming

--- src/BestTimeToParty.hs
+++ src/BestTimeToParty.hs
@@ -1,15 +1,21 @@
 module BestTimeToParty where
 
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
 type Time     = Int
 type Schedule = (Time, Time)
 
 bestTimeToParty :: [Schedule] -> String
-bestTimeToParty = mkMsg . gonyogonyo
+bestTimeToParty = mkMsg . bestTiming . mkTimingSheet
 
 mkMsg :: (Time, Int) -> String
 mkMsg (t, n) = "Best time to attend the party is at "
             ++ show t ++ " o'clock : "
             ++ show n ++ " celebrities will be attending!"
 
-gonyogonyo :: [Schedule] -> (Time, Int)
-gonyogonyo = undefined
+bestTiming :: [(Time, Int)] -> (Time, Int)
+bestTiming = maximumBy (comparing snd)
+
+mkTimingSheet :: [Schedule] -> [(Time, Int)]
+mkTimingSheet = undefined
