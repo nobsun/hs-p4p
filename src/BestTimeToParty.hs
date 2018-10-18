8614af4f3ddbb5c605baa5c9638f1c93e52324fa
Nobuo Yamashita <nobsun@sampou.org>
2018-10-19 06:44:57 +0900

    implement mkMsg

--- src/BestTimeToParty.hs
+++ src/BestTimeToParty.hs
@@ -4,4 +4,12 @@ type Time     = Int
 type Schedule = (Time, Time)
 
 bestTimeToParty :: [Schedule] -> String
-bestTimeToParty = undefined
+bestTimeToParty = mkMsg . gonyogonyo
+
+mkMsg :: (Time, Int) -> String
+mkMsg (t, n) = "Best time to attend the party is at "
+            ++ show t ++ " o'clock : "
+            ++ show n ++ " celebrities will be attending!"
+
+gonyogonyo :: [Schedule] -> (Time, Int)
+gonyogonyo = undefined
--- test/BestTimeToPartySpec.hs
+++ test/BestTimeToPartySpec.hs
@@ -15,15 +15,26 @@ sched = [(6,8),(6,12),(6,7),(7,8)
         ,(7,10),(8,9),(8,10),(9,12)
         ,(9,10),(10,11),(10,12),(11,12)
         ]
-        
+
+timing :: (Time, Int)
+timing = (9, 5)
+
 -- テストコード
 spec :: Spec
 spec = do
-  { bestTimeToPartySpec
+  { mkMsgSpec
+  ; bestTimeToPartySpec
+  }
+
+mkMsgSpec :: Spec
+mkMsgSpec = describe "mkMsg" $ do
+  { it "can make a message from a pair of timing and a number of celebrities" $ do
+    { mkMsg timing `shouldBe` "Best time to attend the party is at 9 o'clock : 5 celebrities will be attending!"
+    }
   }
 
 bestTimeToPartySpec :: Spec
-bestTimeToPartySpec = describe "bestTimeToPartySpec" $ do
+bestTimeToPartySpec = describe "bestTimeToParty" $ do
   { it "can tell us the best time to party from a list of schedules" $ do
     { bestTimeToParty sched `shouldBe` "Best time to attend the party is at 9 o'clock : 5 celebrities will be attending!"
     }
